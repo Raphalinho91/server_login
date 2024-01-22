80ef27b68ef99c5c72f8aa86016e9f1cf9065c99
Raphalinho91 <lokiju1598@outlook.fr>
2024-01-22 14:19:12 +0100

    db mise a jour

--- config/db.js
+++ config/db.js
@@ -6,8 +6,6 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
       serverApi: {
         version: '1',
